0ae39113faccddca3ac65df8e8fcd932b1297f61
squirrelSWJ <squirrelswj@gmail.com>
2016-10-18 15:25:24 +0900

    first commit

---
+++ app/src/main/java/com/example/graduation/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.graduation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity{
+
+    EditText ET_NAME,ET_PASS;
+    String login_name,login_pass;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ET_NAME = (EditText) findViewById(R.id.user_name);
+        ET_PASS = (EditText) findViewById(R.id.user_password);
+    }
+
+    public void userReg(View view) {
+        startActivity(new Intent(this,Register.class));
+    }
+
+    public void userLogin(View view) {
+        login_name = ET_NAME.getText().toString();
+        login_pass = ET_PASS.getText().toString();
+        String method = "login";
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(method, login_name, login_pass);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/graduation/Register.java
@@ -0,0 +1,38 @@
+package com.example.graduation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by 정성욱 on 2016-10-18.
+ */
+public class Register extends Activity{
+
+    EditText ET_NAME,ET_USER_NAME,ET_USER_PASS;
+    String name,user_name,user_pass;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+        ET_NAME = (EditText) findViewById(R.id.name);
+        ET_USER_NAME = (EditText) findViewById(R.id.new_user_name);
+        ET_USER_PASS = (EditText) findViewById(R.id.new_user_password);
+
+
+    }
+
+    public void userReg(View view) {
+        name = ET_NAME.getText().toString();
+        user_name = ET_USER_NAME.getText().toString();
+        user_pass = ET_USER_PASS.getText().toString();
+        String method = "register";
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(method, name, user_name, user_pass);
+        finish();
+
+    }
+}
